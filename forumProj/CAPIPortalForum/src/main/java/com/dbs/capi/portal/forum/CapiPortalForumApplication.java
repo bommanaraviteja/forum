3298c14fb8df7dc3cd0590cd0e5d093e5abe3db6
bommanaraviteja <bommana.raviteja@gmail.com>
2017-09-11 22:53:36 +0530

    Base Version

---
+++ forumProj/CAPIPortalForum/src/main/java/com/dbs/capi/portal/forum/CapiPortalForumApplication.java
@@ -0,0 +1,12 @@
+package com.dbs.capi.portal.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapiPortalForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapiPortalForumApplication.class, args);
+	}
+}
---
+++ forumProj/CAPIPortalForum/src/test/java/com/dbs/capi/portal/forum/CapiPortalForumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbs.capi.portal.forum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapiPortalForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
